45e7fbe6d7393f180204d685b33fa7a2feb9d6c8
mutishimahsan <mutishimahsan@gmail.com>
2026-01-11 23:06:13 +0500

    Day 1 Project, clean architecture, git setup

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add infrastructure services here
+            return services;
+        }
+    }
+}
